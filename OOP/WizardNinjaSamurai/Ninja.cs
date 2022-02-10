ed7681938336167f3db7ca0da8c7508e3e3637ad
Shirley Li <lishirley@me.com>
2022-02-10 12:32:13 -0800

    added steal method

--- OOP/WizardNinjaSamurai/Ninja.cs
+++ OOP/WizardNinjaSamurai/Ninja.cs
@@ -24,5 +24,14 @@ namespace WizardNinjaSamurai
             Console.WriteLine($"{Name} attacked {target.Name} and dealt {damage} damage! {target.Name} now has {target.Health} health.");
             return target.Health;
         }
+
+        // Ninja should have a method called Steal, reduces a target Human's health by 5 and adds this amount to its own health.
+        public int Steal(Human target)
+        {
+            target.Health -= 5;
+            Health += 5;
+            Console.WriteLine($"{Name} stole 5 health from {target.Name}! {Name} now has {Health} health. {target.Name} now has {target.Health} health.") ;
+            return target.Health;
+        }
     }
 }
