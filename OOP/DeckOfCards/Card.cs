1205d9d39bb6dec35ff6aa395267bff16490ac10
Shirley Li <lishirley@me.com>
2022-02-09 21:02:07 -0800

    added Card class

--- OOP/DeckOfCards/Card.cs
+++ OOP/DeckOfCards/Card.cs
@@ -1,4 +1,19 @@
 namespace DeckOfCards
 {
-    
+    public class Card
+    {
+        public string CardFace {get;set;}
+
+        public string Suit {get;set;}
+
+        public int Value {get;set;}
+
+        public Card(string cardFace, string suit, int value)
+        {
+            CardFace = cardFace;
+            Suit = suit;
+            Value = value;
+        }
+
+    }
 }
